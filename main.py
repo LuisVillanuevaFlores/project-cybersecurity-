f433abd23678d405034d05ffcad351144a34bf53
Eduardo Sanchez <eduardo@apurata.com>
2022-10-15 20:34:17 -0500

    obtain relevant data of certificate

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-from crypt import methods
 import os
-import ssl
-from flask import Flask, flash, render_template, session, redirect
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileAllowed
-from flask_bootstrap import Bootstrap
-from wtforms.fields import URLField, SubmitField, FileField
-from werkzeug.utils import secure_filename
 import re
 
+from asn1crypto import pem
+from asn1crypto.x509 import Certificate
+from certvalidator import CertificateValidator
+from flask import Flask
+from flask import flash
+from flask import redirect
+from flask import render_template
+from flask import session
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
+from oscrypto import tls
+from werkzeug.utils import secure_filename
 from wtforms import SubmitField
-from flask_wtf.file import FileField, FileAllowed
-import OpenSSL
+from wtforms.fields import FileField
+from wtforms.fields import SubmitField
+from wtforms.fields import URLField
+
+CERTIFICATES = {}
 
 app = Flask(__name__,
  template_folder='./templates',
  static_folder='./static')
 
 bootstrap = Bootstrap(app)
+
 
 class URLForm(FlaskForm):
     url = URLField('url')
@@ -31,13 +39,42 @@ def verified_https(url):
     return re.match(url_pattern, url)
 
 
-def get_trust_level(url):
-    cert = ssl.get_server_certificate(('www.viabcp.com', 443))
-    print(cert)
-    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
-    # print(x509.get_subject().get_components())
-    print(x509.get_issuer().get_components())
-    return
+def get_relevant_certificate_data(url):
+    host = re.search(r'https://([^/?:]*)', url).group(1)
+    try:
+        connection = tls.TLSSocket(host, 443, session=tls.TLSSession(manual_validation=True))
+    except Exception as e:
+        return None
+    certificate_data = dict(
+        in_mozilla=False,
+        in_chrome=False,
+        in_edge=False,
+        mozilla_trust_level=1,
+        chrome_trust_level=1,
+        edge_trust_level=1,
+    )
+    validator = CertificateValidator(connection.certificate, connection.intermediates)
+
+    certification_chain = validator.validate_tls(connection.hostname)
+    root_certificate = certification_chain[0]
+
+    for mozilla_certificate in CERTIFICATES.get('mozilla_certificates'):
+        if mozilla_certificate.key_identifier_value == root_certificate.key_identifier_value:
+            certificate_data.update({
+                'in_mozilla': True
+            })
+    for chrome_certificate in CERTIFICATES.get('chrome_certificates'):
+        if chrome_certificate.key_identifier_value == root_certificate.key_identifier_value:
+            certificate_data.update({
+                'in_chrome': True
+            })
+    for edge_certificate in CERTIFICATES.get('edge_certificates'):
+        if edge_certificate.key_identifier_value == root_certificate.key_identifier_value:
+            certificate_data.update({
+                'in_edge': True
+            })
+    certificate_data['name'] = root_certificate.subject.human_friendly
+    return certificate_data
 
 
 def validate_file(file):
@@ -53,6 +90,28 @@ def validate_file(file):
             return urls
     else:
         flash(f'El archivo {file.filename} no es un archivo de texto plano .txt')
+
+
+def file_to_certificate_object_list(filename=None):
+    if filename is None:
+        return []
+    certificates_list = []
+    with open(f'./certificates/{filename}', 'rb') as f:
+        for _, _, der_bytes in pem.unarmor(f.read(), multiple=True):
+            certificates_list.append(Certificate.load(der_bytes))
+
+    return certificates_list
+
+
+def load_certificates():
+    if CERTIFICATES.get('has_certificates'):
+        return
+    CERTIFICATES.update({
+        'mozilla_certificates': file_to_certificate_object_list('mozilla_certificates.txt'),
+        'chrome_certificates': file_to_certificate_object_list(),
+        'edge_certificates': file_to_certificate_object_list(),
+        'has_certificates': True,
+    })
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -77,7 +136,7 @@ def index():
         session['urls']=urls
         session['url'] = url
         return redirect('/')
-    trust_level = get_trust_level(url)
+    trust_level = get_relevant_certificate_data(url)
     print(trust_level)
     return render_template('index.html', **context)
 
@@ -86,4 +145,5 @@ if __name__=='__main__':
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['ENV']='development'
     app.config['SECRET_KEY']='KEY_SECRET'
+    load_certificates()
     app.run(debug=True)
--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 from oscrypto import tls
 from certvalidator import CertificateValidator, errors
+from certvalidator.path import ValidationPath
+from asn1crypto.x509 import Certificate, Name
 
 session = tls.TLSSession(manual_validation=True)
 try:
@@ -10,9 +12,13 @@ except Exception as e:
 try:
     validator = CertificateValidator(connection.certificate, connection.intermediates)
     result = validator.validate_tls(connection.hostname)
-    cert_1 = result._getitem_(0)
-    cert_2 = result._getitem_(1)
-    cert_3 = result._getitem_(2)
-    print(hex(cert_1.serial_number), hex(cert_2.serial_number), hex(cert_3.serial_number))
+    # cert_1 = result.__getitem__(0)
+    # cert_2 = result.__getitem__(1)
+    # cert_3 = result.__getitem__(2)
+
+    for cert in result:
+        print(cert.subject.human_friendly)
+
+    # print(hex(cert_1.serial_number), hex(cert_2.serial_number), hex(cert_3.serial_number))
 except (errors.PathValidationError):
     print("The certificate did not match the hostname, or could not be otherwise validated")
